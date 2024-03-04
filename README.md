642b6cee7440380a51a34f68f81157bbd327526d
Austin Barwick <austin.barwick@hey.com>
2024-03-03 18:48:37 -0800

    Add fugitive

---
+++ README.md
@@ -0,0 +1,21 @@
+# Salesforce, Meet Neovim
+
+## The problem
+
+You're involved with Salesforce development, and have a pestering itch to 
+use Neovim with your day to day work. You search on Github and Reddit, but
+all you find our premade Salesforce plugins instead of clear and simple guidance
+on setting up your development environment.
+
+## The solution
+
+This repo will set you up with various flavors of "complete", from a minimal
+config designed to help through the hard parts (LSPs and Treesitter), to
+a full fledged IDE experience complete with Github copilot.
+
+## How do I use this?
+
+This default (main) branch has nothing but instructions, so switch to one
+of the other branches like `minimal` or `ide` to get started. There is
+no license, so feel free to steal as much as you want.
+
--- init.lua
+++ init.lua
@@ -20,44 +20,44 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.filetype.add({
-	extension = {
-		cls = 'apex',
-		apex = 'apex',
-		trigger = 'apex',
-		soql = 'soql',
-		sosl = 'sosl',
-	}
+    extension = {
+        cls = 'apex',
+        apex = 'apex',
+        trigger = 'apex',
+        soql = 'soql',
+        sosl = 'sosl',
+    }
 })
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-	vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
-	{
-		'folke/which-key.nvim',
-		event = 'VimEnter', 
-		config = function()
-			require('which-key').setup()
+    {
+        'folke/which-key.nvim',
+        event = 'VimEnter', 
+        config = function()
+            require('which-key').setup()
 
-			require('which-key').register {
-				['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-				['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-				['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-				['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-				['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-			}
-		end,
-	},
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		requires = {
-			{ "nvim-lua/plenary.nvim" }
-		},
+            require('which-key').register {
+                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+                ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+            }
+        end,
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = {
+            { "nvim-lua/plenary.nvim" }
+        },
         config = function ()
             local harpoon = require("harpoon")
             harpoon:setup()
@@ -67,210 +67,216 @@ require('lazy').setup {
             vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
             vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
             vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
- 
+
             vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
             vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
         end
-	},
-	{ 
-		'nvim-telescope/telescope.nvim',
-		event = 'VimEnter',
-		branch = '0.1.x',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			{
-				'nvim-telescope/telescope-fzf-native.nvim',
+    },
+    { 
+        'nvim-telescope/telescope.nvim',
+        event = 'VimEnter',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
 
-				build = 'make',
+                build = 'make',
 
-				cond = function()
-					return vim.fn.executable 'make' == 1
-				end,
-			},
-			{ 'nvim-telescope/telescope-ui-select.nvim' },
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+            },
+            { 'nvim-telescope/telescope-ui-select.nvim' },
 
-		},
-		config = function()
-			require('telescope').setup {
-				extensions = {
-					['ui-select'] = {
-						require('telescope.themes').get_dropdown(),
-					},
-				},
-			}
-			require'plenary.filetype'.add_file('sf_types')
+        },
+        config = function()
+            require('telescope').setup {
+                extensions = {
+                    ['ui-select'] = {
+                        require('telescope.themes').get_dropdown(),
+                    },
+                },
+            }
+            require'plenary.filetype'.add_file('sf_types')
 
-			pcall(require('telescope').load_extension, 'fzf')
-			pcall(require('telescope').load_extension, 'ui-select')
+            pcall(require('telescope').load_extension, 'fzf')
+            pcall(require('telescope').load_extension, 'ui-select')
 
-			local builtin = require 'telescope.builtin'
-			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-			vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-			vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-			vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-			vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-			vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-			vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-			vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-			vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch existing [B]uffers' })
-			vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Search Files' })
+            local builtin = require 'telescope.builtin'
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+            vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+            vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+            vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+            vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+            vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch existing [B]uffers' })
+            vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Search Files' })
 
-			vim.keymap.set('n', '<leader>/', function()
-				builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-					winblend = 10,
-					previewer = false,
-				})
-			end, { desc = '[/] Fuzzily search in current buffer' })
+            vim.keymap.set('n', '<leader>/', function()
+                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 10,
+                    previewer = false,
+                })
+            end, { desc = '[/] Fuzzily search in current buffer' })
 
-			vim.keymap.set('n', '<leader>s/', function()
-				builtin.live_grep {
-					grep_open_files = true,
-					prompt_title = 'Live Grep in Open Files',
-				}
-			end, { desc = '[S]earch [/] in Open Files' })
+            vim.keymap.set('n', '<leader>s/', function()
+                builtin.live_grep {
+                    grep_open_files = true,
+                    prompt_title = 'Live Grep in Open Files',
+                }
+            end, { desc = '[S]earch [/] in Open Files' })
 
-			vim.keymap.set('n', '<leader>sn', function()
-				builtin.find_files { cwd = vim.fn.stdpath 'config' }
-			end, { desc = '[S]earch [N]eovim files' })
-		end,
-	},
-	{ 
-		"nvim-treesitter/nvim-treesitter", 
-		build = ":TSUpdate",
-		config = function()
-			require('nvim-treesitter.configs').setup({
-				ensure_installed = { 'apex', 'bash', 'rust', 'soql', 'sosl', 'lua', 'vim' },
-				auto_install = true,
+            vim.keymap.set('n', '<leader>sn', function()
+                builtin.find_files { cwd = vim.fn.stdpath 'config' }
+            end, { desc = '[S]earch [N]eovim files' })
+        end,
+    },
+    { 
+        "nvim-treesitter/nvim-treesitter", 
+        build = ":TSUpdate",
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = { 'apex', 'bash', 'rust', 'soql', 'sosl', 'lua', 'vim' },
+                auto_install = true,
 
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				}
-			})
-		end
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/nvim-cmp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"j-hui/fidget.nvim",
-		},
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "j-hui/fidget.nvim",
+        },
 
-		config = function()
-			local on_attach = function(client, bufnr)
-				local toggleInlay = function()
-					if client.server_capabilities.inlayHintProvider then
-						local current_value = vim.lsp.inlay_hint.get({ bufnr = 0 })[1]
-						vim.lsp.inlay_hint.enable(bufnr, not current_value)
-					end
-				end
-			end
-			local cmp = require('cmp')
-			local cmp_lsp = require("cmp_nvim_lsp")
-			local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities())
+        config = function()
+            local on_attach = function(client, bufnr)
+                local toggleInlay = function()
+                    if client.server_capabilities.inlayHintProvider then
+                        local current_value = vim.lsp.inlay_hint.get({ bufnr = 0 })[1]
+                        vim.lsp.inlay_hint.enable(bufnr, not current_value)
+                    end
+                end
+            end
+            local cmp = require('cmp')
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
 
-			require("fidget").setup({})
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"rust_analyzer",
-					"tsserver",
-				},
-				handlers = {
-					function(server_name) -- default handler (optional)
+            require("fidget").setup({})
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "rust_analyzer",
+                    "tsserver",
+                },
+                handlers = {
+                    function(server_name) -- default handler (optional)
 
-						require("lspconfig")[server_name].setup {
-							capabilities = capabilities
-						}
-					end,
+                        require("lspconfig")[server_name].setup {
+                            capabilities = capabilities
+                        }
+                    end,
 
-					["lua_ls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.lua_ls.setup {
-							capabilities = capabilities,
-							settings = {
-								Lua = {
-									diagnostics = {
-										globals = { "vim", "it", "describe", "before_each", "after_each" },
-									}
-								}
-							}
-						}
-					end,
-				}
-			})
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup {
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    diagnostics = {
+                                        globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                    }
+                                }
+                            }
+                        }
+                    end,
+                }
+            })
 
-			local lspconfig = require 'lspconfig'
-			lspconfig.apex_ls.setup {
-				apex_enable_semantic_errors = false,
-				apex_enable_completion_statistics = false,
-				filetypes = { 'apex' },
-				root_dir = lspconfig.util.root_pattern('sfdx-project.json'),
+            local lspconfig = require 'lspconfig'
+            lspconfig.apex_ls.setup {
+                apex_enable_semantic_errors = false,
+                apex_enable_completion_statistics = false,
+                filetypes = { 'apex' },
+                root_dir = lspconfig.util.root_pattern('sfdx-project.json'),
 
-				on_attach = on_attach,
-				capabilities = capabilities,
-			}	
+                on_attach = on_attach,
+                capabilities = capabilities,
+            }	
 
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-					['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-					['<C-y>'] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' }, -- For luasnip users.
-				}, {
-					{ name = 'buffer' },
-				})
-			})
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' }, -- For luasnip users.
+                }, {
+                    { name = 'buffer' },
+                })
+            })
 
-			vim.diagnostic.config({
-				-- update_in_insert = true,
-				float = {
-					focusable = false,
-					style = "minimal",
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-				},
-			})
-		end
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},	
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {}
-	}
+            vim.diagnostic.config({
+                -- update_in_insert = true,
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            })
+        end
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},	
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {}
+    },
+    {
+        "tpope/vim-fugitive",
+        config = function ()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        end
+    }
 }
 
 vim.cmd 'colorscheme kanagawa'
